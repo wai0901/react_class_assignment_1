4762fbaae2e52fb2093f11b57885a314d962b72e
Kent leung <snail@hotmail.com>
2020-04-10 00:31:18 -0700

    added Redux Thunk

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,42 +1,6 @@
-import React, { Component, useState } from "react";
-import { Card, Row, Col, ModalBody, ModalHeader, Label, CardImg, CardImgOverlay, 
-        CardText, CardBody, Modal, Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { Col, Label, Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import {Control, LocalForm, Errors } from 'react-redux-form';
-import { Loading } from './LoadingComponent';
-
-
-   //method for render component for selected campsite pass from parent component.
-   const RenderCampsite = ({campsite}) => {
-    return (
-        <div  key={campsite.id} className="col-md-5 m-1">
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    )
-   }
-
-   //method for render comments from comments array from "this.props.campsite.comments".
-   const RenderComments = ({comments, addComment, campsiteId}) => {
-       return (
-        <div className="col-md-5 m-1">
-            <h4>Comments</h4>
-            {
-                comments.map(comment => 
-                    <p key={comments.id}>
-                        {comment.text}<br /> 
-                        --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                    </p>
-                )
-            }
-            <CommentForm campsiteId={campsiteId} addComment={addComment}/>
-        </div>
-       )
-   }
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -61,8 +25,6 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        this.toggleModal()
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         alert("Current State is: " + JSON.stringify(values));
     }
 
@@ -124,53 +86,4 @@ class CommentForm extends Component {
     }
 }
 
-const CampsiteInfo = (props) => {
-    if (props.isLoading) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Loading />
-                </div>
-            </div>
-        );
-    }
-    if (props.errMess) {
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>{props.errMess}</h4>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    if (props.campsite) {
-    return ( 
-        props.campsite?
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <h2>{props.campsite.name}</h2>
-                    <hr />
-                </div>
-            </div>
-            <div className="row">
-                <RenderCampsite campsite={props.campsite} />
-                <RenderComments 
-                    comments={props.comments}
-                    addComment={props.addComment}
-                    campsiteId={props.campsite.id}
-                    />
-            </div> 
-        </div>:
-        <div></div>        
-    )}
-}
-
-
-export default CampsiteInfo;
+export default CommentForm;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Aboutus from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 import Contact from './ContactComponent';
+import { addComment, fetchCampsites } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -18,17 +19,27 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = {
+  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+  fetchCampsites: () => (fetchCampsites())
+}
+
+
 class Main extends Component {
 
-
+  componentDidMount() {
+    this.props.fetchCampsites();
+}
 
   render() {
 
     const HomePage = () => {
       return (
         <Home 
-          campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
-          comment={this.props.comments.filter(comment => comment.featured)[0]}
+          campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+          campsitesLoading={this.props.campsites.isLoading}
+          campsitesErrMess={this.props.campsites.errMess}
+          // comment={this.props.comments.filter(comment => comment.featured)[0]}
           partner={this.props.partners.filter(partner => partner.featured)[0]}
           promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
         />
@@ -38,8 +49,11 @@ class Main extends Component {
     const CampsiteWithId = ({match}) => {
       return (
         <CampsiteInfo 
-          campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+          campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+          isLoading={this.props.campsites.isLoading}
+          errMess={this.props.campsites.errMess}
           comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+          addComment={this.props.addComment}
         />
       )
     }
@@ -61,4 +75,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
